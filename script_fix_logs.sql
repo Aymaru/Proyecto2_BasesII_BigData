759682c26bbfcf3ef250a0b2775864c200e79507
Unknown <aycastillo@ic-itcr.ac.cr>
2019-06-14 16:21:37 -0600

    Nuevos logs

--- script_fix_logs.sql
+++ script_fix_logs.sql
@@ -4,7 +4,7 @@ alter table dbo.CDB_Logs
 add action_section nvarchar(32);
 
 update dbo.CDB_Logs 
-set action_section = g1.name
+set action_section = RTRIM( g1.name)
 from	dbo.CDB_ActionsInfo ai inner join
 		dbo.CDB_Generics g1 on (ai.idAction = g1.idGeneric) inner join
 		dbo.CDB_ActionsXSection axs on (ai.idAction = axs.idActionSection) inner join
